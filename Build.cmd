cb35ae71aa183ab4c55a5542b0508188b8c9db89
Kalle-Muenster <kalle.muenster@outlook.de>
2022-07-10 02:05:09 +0200

    Correct paths to dependencies

--- Build.cmd
+++ Build.cmd
@@ -17,13 +17,13 @@ set MotorsportsVersionString=0.0.0.1
 
 :: Set Dependencies
 if "%ConsolaBinRoot%"=="" (
-set ConsolaBinRoot=C:\WORKSPACE\PROJECTS\GITSPACE\Consola\bin\%DotNetVersionString%
+set ConsolaBinRoot=.\..\..\Consola\bin\%DotNetVersionString%
 )
 if "%Int24TypesBinRoot%"=="" (
-set Int24TypesBinRoot=C:\WORKSPACE\PROJECTS\GITSPACE\Int24Types\bin\%DotNetVersionString%
+set Int24TypesBinRoot=.\..\..\Int24Types\bin\%DotNetVersionString%
 )
 if "%ControlledValuesBinRoot%"=="" (
-set ControlledValuesBinRoot=C:\WORKSPACE\PROJECTS\GITSPACE\ControlledValues\bin\%DotNetVersionString%
+set ControlledValuesBinRoot=.\..\..\ControlledValues\bin\%DotNetVersionString%
 )
 
 :: Set parameters and solution files
